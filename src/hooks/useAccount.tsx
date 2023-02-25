ab57f75f27baad56cebf5456f72d6ec97b1b7ca9
Cheng Wang <wachmc@gmail.com>
2023-02-25 20:32:45 +0100

    Fix connection exception

--- src/hooks/useAccount.tsx
+++ src/hooks/useAccount.tsx
@@ -1,30 +1,7 @@
-import { connect } from '@alephium/get-extension-wallet'
-import { useEffect } from 'react'
 import { useContext } from '../components/AlephiumConnect';
 
-export function useAccount(
-  networkId: string,
-  onDisconnected: () => Promise<void> = () => Promise.resolve()
-) {
+export function useAccount() {
   const context = useContext()
-
-  useEffect(() => {
-    const handler = async () => {
-      const windowAlephium = await connect({ showList: false })
-      const enabledAccount = await windowAlephium?.enable({ onDisconnected, networkId: networkId })
-
-      if (windowAlephium) {
-        context.setSignerProvider(windowAlephium)
-        if (windowAlephium.connectedNetworkId) {
-          context.setNetwork(windowAlephium.connectedNetworkId)
-        }
-      }
-
-      enabledAccount && context.setAccount(enabledAccount)
-    }
-
-    handler()
-  }, [])
 
   return { account: context.account, isConnected: !!context.account }
 }
--- src/hooks/useConnect.tsx
+++ src/hooks/useConnect.tsx
@@ -1,5 +1,5 @@
 import { connect } from '@alephium/get-extension-wallet'
-import type { Account, EnableOptionsBase } from '@alephium/web3';
+import type { EnableOptionsBase } from '@alephium/web3';
 import { useContext } from '../components/AlephiumConnect';
 import { getAlephium } from '@alephium/get-extension-wallet';
 import { useCallback } from 'react';
@@ -26,16 +26,16 @@ export function useConnect(
       include: ['alephium']
     })
 
-    const enabledAccount = await windowAlephium?.enable({...options, showModal: false, onDisconnected: disconnectAlephium})
+    const enabledAccount = await windowAlephium?.enable({
+      ...options, showModal: false, onDisconnected: disconnectAlephium
+    }).catch(() => undefined) // Need to catch the exception here
 
-    if (windowAlephium) {
+    if (windowAlephium && enabledAccount) {
       context.setSignerProvider(windowAlephium)
-      if (windowAlephium.connectedNetworkId) {
-        context.setNetwork(windowAlephium.connectedNetworkId)
-      }
+      context.setNetwork(options.networkId)
+      context.setAccount(enabledAccount)
     }
 
-    enabledAccount && context.setAccount(enabledAccount)
     return enabledAccount
   }, [context])
 
