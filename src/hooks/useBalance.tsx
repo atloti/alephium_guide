8a0afb893d7e666df4cc8e80a0054745a6efe80a
lbqds <lbqds@outlook.com>
2023-03-21 17:02:55 +0800

    Add a parameter to trigger the get-balance request

--- src/hooks/useBalance.tsx
+++ src/hooks/useBalance.tsx
@@ -2,7 +2,7 @@ import { Balance } from '@alephium/web3/dist/src/api/api-alephium';
 import { useEffect, useState } from 'react';
 import { useContext } from '../components/AlephiumConnect';
 
-export function useBalance() {
+export function useBalance<T>(value?: T) {
   const context = useContext()
   const [balance, setBalance] = useState<Balance>()
 
@@ -16,7 +16,7 @@ export function useBalance() {
     }
 
     handler()
-  }, [context.signerProvider?.nodeProvider, context.account])
+  }, [context.signerProvider?.nodeProvider, context.account, value])
 
   return { balance }
 }
